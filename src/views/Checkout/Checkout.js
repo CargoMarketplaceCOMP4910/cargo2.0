90a99734fffd8263379ca07fde6109acb06b9d8f
Karanveer <karanveersinghgill1@gmail.com>
2020-12-11 14:54:19 -0800

    checkout complete

--- src/views/Checkout/Checkout.js
+++ src/views/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+ import React,{useState} from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
